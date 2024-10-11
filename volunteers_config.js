0402e1fd3ff026ff61c8c60b503e9663e4b46c3e
Carly Burkhart <21320169+TimeIsOnMySide@users.noreply.github.com>
2024-10-10 19:47:37 -0700

    added volunteer tiles. Made 3 columns of volunteers

--- volunteers_config.js
+++ volunteers_config.js
@@ -2,6 +2,12 @@ const folderNames = [
   'tahnee_joyce_mccoy',
   'carly_kennedy',
   'carmen_villalobos',
+  'sasha_garcia',
+  'julie_knowles',
+  'kelly_gaines',
+  'erica_gates',
+  'jenni_preciado',
+  'jemadean_vasquez',
 
   // Add more folder names as needed
 ].sort();
